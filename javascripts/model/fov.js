00e266c247c7783afa04debd7662b9bbb46d8983
Michael Leßnau <michael.lessnau@gmail.com>
2015-01-04 03:22:52 +0100

    changes color of FOV controls reset buttons

--- javascripts/model/fov.js
+++ javascripts/model/fov.js
@@ -13,11 +13,11 @@ Fov.prototype.initialize = function () {
 Fov.prototype.initializeEventHandlers = function () {
   var self = this;
 
-  this.eventMediator.subscribe("fov:translate:decrement-x", function () { self.translate.x -= 10; self.setChanged(); });
-  this.eventMediator.subscribe("fov:translate:increment-x", function () { self.translate.x += 10; self.setChanged(); });
+  this.eventMediator.subscribe("fov:translate:decrement-x", function () { self.translate.x -= 10 / self.scale; self.setChanged(); });
+  this.eventMediator.subscribe("fov:translate:increment-x", function () { self.translate.x += 10 / self.scale; self.setChanged(); });
   this.eventMediator.subscribe("fov:translate:reset", function () { self.translate = {x: 0, y: 0}; self.setChanged(); });
-  this.eventMediator.subscribe("fov:translate:decrement-y", function () { self.translate.y -= 10; self.setChanged(); });
-  this.eventMediator.subscribe("fov:translate:increment-y", function () { self.translate.y += 10; self.setChanged(); });
+  this.eventMediator.subscribe("fov:translate:decrement-y", function () { self.translate.y -= 10 / self.scale; self.setChanged(); });
+  this.eventMediator.subscribe("fov:translate:increment-y", function () { self.translate.y += 10 / self.scale; self.setChanged(); });
   this.eventMediator.subscribe("fov:scale:decrement", function () { self.scale *= 0.5; self.setChanged(); });
   this.eventMediator.subscribe("fov:scale:reset", function () { self.scale = 1; self.setChanged(); });
   this.eventMediator.subscribe("fov:scale:increment", function () { self.scale /= 0.5; self.setChanged(); });
